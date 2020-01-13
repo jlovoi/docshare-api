e7261a2c272849f27566f98d73f3c3ee1796685b
jlovoi <joelovoi@gmail.com>
2020-01-12 22:45:02 -0600

    implemented delete

---
+++ src/collections/users/delete.js
@@ -0,0 +1,12 @@
+const ObjectID = require("mongodb").ObjectID;
+
+module.exports = (app, db) => {
+  app.delete("/users/:id", (req, res) => {
+    db.collection("users").deleteOne({ _id: ObjectID(req.params.id) }, err => {
+      if (err) {
+        return console.log("Error deleting user: ", err);
+      }
+      res.send({ data: `deleted document ${req.params.id}` });
+    });
+  });
+};
--- src/collections/users/index.js
+++ src/collections/users/index.js
@@ -2,10 +2,12 @@ const usersPost = require("./post");
 const usersPatch = require("./patch");
 const usersGet = require("./get");
 const usersAll = require("./all");
+const usersDelete = require("./delete");
 
 module.exports = (app, db) => {
   usersPost(app, db);
   usersPatch(app, db);
   usersGet(app, db);
   usersAll(app, db);
+  usersDelete(app, db);
 };
