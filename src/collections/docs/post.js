aa8ec85e15562128935d6dad8af062f5dd50d7ad
jlovoi <joelovoi@gmail.com>
2020-02-20 10:39:44 -0700

    posting documents

--- src/collections/docs/post.js
+++ src/collections/docs/post.js
@@ -1,12 +1,21 @@
+const ObjectID = require("mongodb").ObjectID;
+
 module.exports = (app, db) => {
   app.post("/docs", (req, res) => {
-    db.collection("docs").save(req.body, (err, result) => {
+    console.log(req.body);
+    const body = {
+      ...req.body,
+      content: Buffer.from(req.body.content, "utf8"),
+      users: req.body.users.map(id => ObjectID(id)),
+      createdAt: new Date(),
+      createdBy: ObjectID(req.body.createdBy)
+    };
+    db.collection("docs").insertOne(body, (err, result) => {
       if (err) {
         return console.error("Error posting doc: ", err);
       }
 
-      console.log("saved to database");
-      res.redirect("/");
+      res.send({ data: result.ops[0] });
     });
   });
 };
